9b867277137faf90d6964d2e19cdd1b36d06a5e4
Ljubica Kovacevic <ljubicak1996@gmail.com>
2017-11-09 22:15:58 +0100

    Made the app show a map of Sarajevo

--- Map Practice/ViewController.swift
+++ Map Practice/ViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let distanceSpan: CLLocationDegrees = 2000
+        let sarajevoLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(43.85625, 18.41307)
+        
+        mapView.setRegion(MKCoordinateRegionMakeWithDistance(sarajevoLocation, distanceSpan, distanceSpan), animated: true)
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
